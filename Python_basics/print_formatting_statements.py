72b05d91b1203902f25c958ce5947351dad86e72
Rushikesh Joharkar <88484461+rushikeshjoharkar7@users.noreply.github.com>
2024-01-05 13:14:36 +0100

    Add files via upload

---
+++ Python_basics/print_formatting_statements.py
@@ -0,0 +1,13 @@
+place = "Pune, hinjewadi"
+experience = 2
+company = "HCL Tech"
+
+print("My place is " + place + ". I have around", experience, "years of experience at " + company)
+
+print("My place is {}. I have around {} years of experience at {}".format(place, experience, company))
+
+print("My place is {0}. I have around {1} years of experience at {2}".format(place, experience, company))
+
+print("My place is {2}. I have around {1} years of experience at {0}".format(company, experience, place))
+
+print("My place is %s. I have around %d years of experience at %s" % (place, experience, company))
