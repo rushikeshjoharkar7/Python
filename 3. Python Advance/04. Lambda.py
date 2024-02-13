47301552ce45b31a40cd722bb26173b0d5d6dcb6
Rushikesh Joharkar <88484461+rushikeshjoharkar7@users.noreply.github.com>
2024-02-13 18:12:08 +0530

    Add files via upload

---
+++ 3. Python Advance/04. Lambda.py
@@ -0,0 +1,21 @@
+"""
+Lambda function in python:
+
+- A function which has no name and can intake any number of arguments
+     and one expression is lambda function.
+
+ - Lambda function reduces the number of lines of code to be written in a normal function.
+"""
+################################################################################################
+
+sum = lambda a, b: a + b
+print(sum(2, 3))
+
+mul = lambda m : m ** 2
+print(mul(21))
+
+"""
+o/p:
+5
+441
+"""
